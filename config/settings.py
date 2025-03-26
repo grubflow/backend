3be688644bff9a4ebb2aa58bbfabc1766d1fd18a
Colinster327 <cmurph0327@gmail.com>
2025-03-26 16:23:58 -0400

    add swagger endpoint documentation

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
