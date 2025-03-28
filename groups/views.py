de82bbaa994245fca41be02af68ec0272c0a5094
Colin Murphy <66136651+Colinster327@users.noreply.github.com>
2025-03-27 21:49:54 -0400

    add leave group invite (#6)

--- groups/views.py
+++ groups/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.mixins import (CreateModelMixin, ListModelMixin,
                                    UpdateModelMixin)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from common.permissions import IsOwnerOrAdmin
 
@@ -12,7 +15,12 @@ from .serializers import (GroupListSerializer, GroupSerializer,
 
 
 class GroupViewset(viewsets.ModelViewSet):
-    permission_classes = [IsOwnerOrAdmin]
+    def get_permissions(self):
+        if self.action == 'leave':
+            permissions = [IsAuthenticated]
+        else:
+            permissions = [IsOwnerOrAdmin]
+        return [permission() for permission in permissions]
 
     def get_queryset(self):
         return Group.objects.filter(members=self.request.user)
@@ -26,6 +34,20 @@ class GroupViewset(viewsets.ModelViewSet):
         group = serializer.save(owner_username=self.request.user)
         group.members.add(self.request.user)
         group.save()
+
+    @action(detail=False, methods=['delete'])
+    def leave(self, request):
+        group = request.data.get('group', None)
+        if not group:
+            return Response({"detail": "group parameter is required."}, status=400)
+        elif not Group.objects.filter(composite_key=group, members=request.user).exists():
+            return Response({"detail": "You are not a member of this group."}, status=400)
+
+        group_instance = Group.objects.get(composite_key=group)
+        group_instance.members.remove(request.user)
+        group_instance.save()
+
+        return Response(status=204)
 
 
 class SendGroupInviteViewset(CreateModelMixin, ListModelMixin, UpdateModelMixin, viewsets.GenericViewSet):
@@ -43,6 +65,20 @@ class SendGroupInviteViewset(CreateModelMixin, ListModelMixin, UpdateModelMixin,
         return SendGroupInviteCreateSerializer
 
     def create(self, request, *args, **kwargs):
+        receiving_username = request.data.get('receiving_username')
+        group = request.data.get('group')
+        existing_invite = SendGroupInvite.objects.filter(
+            sending_username=request.user,
+            group__composite_key=group,
+            receiving_username__username=receiving_username,
+        ).first()
+        existing_group = Group.objects.filter(
+            composite_key=group,
+            members__username=receiving_username
+        ).first()
+        if existing_invite and not existing_group:
+            existing_invite.delete()
+
         request.data['sending_username'] = request.user.username
         return super().create(request, *args, **kwargs)
 
