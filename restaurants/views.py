21ddb34a14b6ad5df28bdd11b8bbd00fdc5c7754
BrockK64 <brock.klamfoth50@gmail.com>
2025-04-04 15:05:31 -0400

    Removed duplicate code

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework import viewsets
 from common.permissions import AdminWriteElseAuthenticated
 from .models import Restaurant
 from .serializers import RestaurantSerializer
